ca578c21218edaf8c443cb4ff1c8f2886cc85e9c
CookieGamer733 <74946768+CookieGamer733@users.noreply.github.com>
2021-01-26 21:28:24 -0600

    Deleted unnecessary features

--- permissions.ts
+++
@@ -1,9 +0,0 @@
-import { Message } from "discord.js";
-
-const ownerID = process.env.OWNERID!;
-
-export const permissions = new Map<string, (message: Message) => boolean>()
-  .set("lv4", message => (message.author.id === ownerID))
-  .set("lv3", message => Boolean(message.member && message.member.permissions.has("ADMINISTRATOR") || (message.author.id === ownerID)))
-  .set("lv2", message => Boolean(message.member && message.member.permissions.has("MANAGE_MESSAGES"))) /* Moderator */
-  .set("lv1", _ => true);
