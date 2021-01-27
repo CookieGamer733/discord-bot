d93c4eff857ef0a8bbf3e71509b0e93a40173a15
CookieGamer733 <74946768+CookieGamer733@users.noreply.github.com>
2021-01-26 21:26:06 -0600

    Rewrote Code
    Cleaned up a bunch of the code, and deleted unnecessary functions.

--- README.md
+++ README.md
@@ -3,22 +3,28 @@
 ### Instructions for Code Installation
 
 
-#### Step 1: ENV Values
+#### Step 1: Config Values
+There are two options:
+1. Use an ENV (recommended if your code is running publicly)
+2. Use a config.json (don't use if your code is running publicly)
+
+###### If using an ENV
 Go to .env and add the following data:
  - TOKEN: This is your bot token.
  - PREFIX: Put the prefix you want for your bot.
  - OWNERID: This is your Discord ID.
- - MONGOURL: This is your URL for connecting to the database. This steps to setting this up is down below.
+After doing this, go through each file and replace config.<VALUE> with
+###### If using a config.json
+Go to config.json and add the following data:
+ - TOKEN: This is your bot token.
+ - PREFIX: Put the prefix you want for your bot.
+ - OWNERID: This is your Discord ID.
+If you do this, you can delete the .env file.
 
-#### Step 2: Mongo Database
-Go to [Mongo Database](https://account.mongodb.com/account/login) and login to your account, or create an account if you don't have one.
-Create a cluster ([Instructions](https://docs.atlas.mongodb.com/tutorial/deploy-free-tier-cluster)).
-Add your IP address, or if you are using [repl.it](https://repl.it), click the no whitelist option ([Instructions](https://docs.atlas.mongodb.com/security/add-ip-address-to-list)).
-Create a Database User ([Instructions](https://docs.atlas.mongodb.com/tutorial/create-mongodb-user-for-cluster)).
-Connect to the Cluster ([Instructions](https://docs.atlas.mongodb.com/tutorial/connect-to-your-cluster)). Make sure you click Node.js driver. You should get an Atlas connection string. This is what MONGOURL in the ENV should be.
+#### Complete
+If you did the steps above correctly, you should be able to go run your discord bot no problem.
 
 ### File breakdown: What is each file?
-
  - index.ts
     - This is where a majority of the bots running code is. This is the main script.
  - .env
@@ -27,11 +33,8 @@ Connect to the Cluster ([Instructions](https://docs.atlas.mongodb.com/tutorial/c
     - This is the handler for connecting to the Mongo database.
  - LICENSE
     - This is the licensing for my code.
-      **WARNING: UNDER NO CIRCUMSTANCES ARE YOU TO DELETE THIS FILE**
  - tsconfig.json
     - This is the TypeScript config file. I would not recommend touching anything in here unless you know what you are doing.
- - xphandler.ts
-    - This is the handler for the XP functions
  - commands/
     - disable.ts
        - Use this command to disable any command (usually if that particular command is malfunctioning)
@@ -45,6 +48,3 @@ Connect to the Cluster ([Instructions](https://docs.atlas.mongodb.com/tutorial/c
        - This is a developer command. If you know how to use it, great! If you don't know how to use it, you don't need to use it.
     - kill.ts
        - This shuts down the bot. Simple as that.
-    - xp.ts
-       - This is used to show the xp of a user
-          - Usage: xp <optional mention>
--- index.ts
+++ index.ts
@@ -1,8 +1,7 @@
-import "array-flat-polyfill";
-import "./database";
 import { Client, Message } from "discord.js";
-import { permissions } from "./permissions";
 import { readdirSync } from "fs";
+
+const config = require("./config.json");
 
 export const client = new Client({
   disableMentions: "everyone"
@@ -11,12 +10,11 @@ interface Command {
   name: string;
   execute(message: Message, args: string[]): void;
   _delete: boolean; // If true, the message calling the command will be deleted
-  permissions: string;
 };
 const commands = new Map<string, Command>();
 export const disabled = new Set<string>();
-(async () => {
-  for (const file of (await readdirSync(`./commands/`))) {
+(async (): Promise<void> => {
+  for (const file of (readdirSync(`./commands`))) {
     const command: Command = await import(`./commands/${file}`);
     try {
       commands.set(command.name, command);
@@ -26,7 +24,7 @@ export const disabled = new Set<string>();
     }
   }
   try {
-    client.login(process.env.TOKEN!);
+    client.login(config.TOKEN!);
   } catch (e) {
     console.error(e);
   }
@@ -39,43 +37,34 @@ client
     client.user!.setPresence({
       activity: {
         type: "LISTENING",
-        name: `messages with ${process.env.PREFIX!}`,
+        name: `messages with ${config.PREFIX!}`,
       },
       status: "online",
     });
     console.log("Status and activity set");
   })
   .on("message", (message: Message) => {
-    if (!message.content.toLowerCase()
-      .startsWith(process.env.PREFIX!)) return;
-    const args = message.content.slice(process.env.PREFIX!.length)
-      .trim()
-      .split(/\s+/);
+    if (!message.content.toLowerCase().startsWith(config.PREFIX!)) return;
+    const args = message.content.slice(config.PREFIX!.length).trim().split(/\s+/);
     if (!args.length) return;
     const commandName = args[0].toLowerCase();
     args.shift();
     if (!commandName) return;
-    if (disabled.has(commandName))
-      return message.channel.send("That command has been disabled");
+    if (disabled.has(commandName)) return message.channel.send("That command has been disabled");
     if (!commands.has(commandName)) return;
     const command = commands.get(commandName)!;
-    if (!permissions.get(command.permissions || "none")!(message))
-      return message.channel.send("You don't have permission to use that command!");
     command.execute(message, args);
-    if (message.channel.type !== "dm" && !command._delete) message.delete();
+    if (message.channel.type !== "dm" && command._delete) message.delete();
   });
 
 // ***************************************************************************
 // ** All custom actions go here \/ \/ 
 // ***************************************************************************
 
-// ***** XP handler ***** 
-import xphandler_init from "./xphandler";
-xphandler_init(client);
-// ***** Error logging ***** 
-process.on('unhandledRejection', (promise) => {
+// ***** Error logging *****
+process.on('unhandledRejection', (promise): void => {
   console.log(`Unhandled Rejection at:\n`, promise);
 });
-process.on('uncaughtException', (promise) => {
+process.on('uncaughtException', (promise): void => {
   console.log(`Uncaught Exeception at:\n`, promise);
 });
